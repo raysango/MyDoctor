ca34a79dbc11e793d9d44e1aff87c08063dcc14d
Raymond Morcos <remongeorge@live.com>
2018-04-18 22:39:29 -0700

    Adds hash table and linked list

---
+++ app/models/linked_list.rb
@@ -0,0 +1,54 @@
+class LinkedList
+  attr_accessor :head, :tail, :size
+
+  def initialize
+    @size = 0
+    @head = nil
+    @tail = nil
+  end
+
+  def insert(index, data)
+    if @head.present? && @tail.present?
+      if index == 0
+        prepend(data)
+      elsif index == @size
+        append(data)
+      else
+      current = @head
+      (index - 1).times do
+        current = current.next_node
+      end
+      tmp = current.next_node
+      current.next_node = Node.new(data)
+      current.next_node.next_node = tmp
+      end
+    else
+      @head = Node.new(data)
+      @tail = @head
+    end
+    @size += 1
+  end
+
+  def prepend(data)
+    if @head.nil?
+      @head = Node.new(data)
+      @tail = @head
+    else
+      tmp_head = @head
+      @head = Node.new(data)
+      @head.next_node = tmp_head
+    end
+    @size += 1
+  end
+
+  def append(data)
+    if @head.nil?
+      @head = Node.new(data)
+      @tail = @head
+    else
+      @tail.next_node = Node.new(data)
+      @tail = @tail.next_node
+    end
+    @size += 1
+  end
+end
---
+++ app/models/node.rb
@@ -0,0 +1,8 @@
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(data)
+    @value = data
+    @next_node = nil
+  end
+end
